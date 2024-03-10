e319325738e76a93baebb48462a88cf50c96fd26
David_Portillo <88davd@gmail.com>
2024-03-10 13:35:42 +0100

    test img

---
+++ src/main/java/com/winningstation/controller/StaticResourceController.java
@@ -0,0 +1,21 @@
+package com.winningstation.controller;
+
+import org.springframework.core.io.PathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Paths;
+
+@RestController
+public class StaticResourceController {
+
+    @GetMapping("/images/{filename:.+}")
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = new PathResource(Paths.get("/images/", filename));
+        return ResponseEntity.ok().body(file);
+    }
+}
