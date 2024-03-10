d63846b982bd5fd68833c62641605da965b451cc
David_Portillo <88davd@gmail.com>
2024-03-10 13:41:03 +0100

    test img

--- src/main/java/com/winningstation/controller/StaticResourceController.java
+++ src/main/java/com/winningstation/controller/StaticResourceController.java
@@ -2,6 +2,7 @@ package com.winningstation.controller;
 
 import org.springframework.core.io.PathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,10 +13,25 @@ import java.nio.file.Paths;
 @RestController
 public class StaticResourceController {
 
-    @GetMapping("/images/{filename:.+}")
-    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+  @GetMapping("/images/uploads/{filename:.+}")
+  public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
-        Resource file = new PathResource(Paths.get("/images/", filename));
-        return ResponseEntity.ok().body(file);
+    Resource file = new PathResource(Paths.get("/images/", filename));
+    String contentType = getContentType(filename);
+
+    return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(file);
+  }
+
+  private String getContentType(String filename) {
+    String extension = filename.substring(filename.lastIndexOf(".") + 1);
+    switch (extension.toLowerCase()) {
+      case "png":
+        return "image/png";
+      case "jpg":
+      case "jpeg":
+        return "image/jpeg";
+      default:
+        return "application/octet-stream";
     }
+  }
 }
